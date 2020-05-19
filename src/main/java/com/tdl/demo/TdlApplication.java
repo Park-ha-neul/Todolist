2bf18a21e1209b2c10a7d6567ef0bf52200b3703
Park-ha-nule <sodlfma4869@naver.com>
2020-05-19 17:05:10 +0900

    creating kakao api

---
+++ src/main/java/com/tdl/demo/TdlApplication.java
@@ -0,0 +1,15 @@
+package com.tdl.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class TdlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdlApplication.class, args);
+	}
+
+}
